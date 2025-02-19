d1d9665aea30234ebe276268851a402d8e49781d
CYBER-RAJIB <muslimhakcerbd@gmail.com>
2025-02-19 08:53:02 +0600

    Update ...!CYBER-ODIO-🎧.js

--- TINA/commands/...!CYBER-ODIO-🎧.js
+++ TINA/commands/...!CYBER-ODIO-🎧.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   if (event.body.indexOf("🎧")==0 || event.body.indexOf("🎧")==0 || event.body.indexOf("🎧")==0 || event.body.indexOf("🎧")==0) {
     var msg = {
         body: "🖤🎧",
-        attachment: fs.createReadStream(__dirname + `/cyber/npx.mp3`)
+        attachment: fs.createReadStream(__dirname + `/CYBER/tawba.mp3`)
       }
       api.sendMessage( msg, threadID, messageID);
     api.setMessageReaction("❤️‍🩹", event.messageID, (err) => {}, true)
